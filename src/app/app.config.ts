c166ea40e9bf6077089e05aa978f86d2df96afd3
codename399 <gaudajay399@gmail.com>
2026-07-05 11:54:08 +0530

    fixed manifest

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,11 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { Config } from '../assets/environments/config';
 import { paginationRequestFactory } from '../factories/pagination-request-factory';
-import { API_CONSTANTS, CONFIG_FILE_PATH, PAGINATION_REQUEST } from '../injectors/common-injector';
+import {
+  API_CONSTANTS,
+  CONFIG_FILE_PATH,
+  PAGINATION_REQUEST,
+} from '../injectors/common-injector';
 import { authInterceptor } from '../interceptor/auth-interceptor';
 import { routes } from './app.routes';
 import { AuthenticationService } from './components/authentication/services/authentication-service';
@@ -26,9 +30,11 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor, refreshTokenInterceptor])),
+    provideHttpClient(
+      withInterceptors([authInterceptor, refreshTokenInterceptor]),
+    ),
     importProvidersFrom(
-      AuthenticationService // Import any additional modules here, e.g., HttpClientModule, FormsModule, etc.
+      AuthenticationService, // Import any additional modules here, e.g., HttpClientModule, FormsModule, etc.
     ),
     {
       provide: PAGINATION_REQUEST,
@@ -36,7 +42,7 @@ export const appConfig: ApplicationConfig = {
     },
     {
       provide: CONFIG_FILE_PATH,
-      useValue: "/assets/environments/config.json"
+      useValue: '/assets/environments/config.json',
     },
     {
       provide: APP_INITIALIZER,
@@ -44,21 +50,22 @@ export const appConfig: ApplicationConfig = {
         await configService.load();
       },
       deps: [ConfigService],
-      multi: true
+      multi: true,
     },
     {
       provide: Config,
       useFactory: (configService: ConfigService) => {
-        return isDevMode() ? environment as Config : configService.value;
+        return isDevMode() ? (environment as Config) : configService.value;
       },
-      deps: [ConfigService]
+      deps: [ConfigService],
     },
     {
       provide: API_CONSTANTS,
-      useClass: ApiConstants
-    }, provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })
+      useClass: ApiConstants,
+    },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    })
   ],
 };
